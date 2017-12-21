29031d722130e43421e83f5532bee075f5d27546
gozi200 <school_game_sp@yaoo.co.jp>
2017-12-21 15:17:15 +0900

    仕様書にリソースシートを追加 仕様書のプレイアブルキャラクターシートに追記

---
+++ Roguelike_/Assets/Scripts/Game_Object/Actor/Player/Player_Data.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class Player_Data : MonoBehaviour {
+    private class Read_Player_Data {
+        [CsvColumnAttribute(0, 0)]
+        private int id;
+
+        [CsvColumnAttribute(1, "NoName")]
+        public string Name {
+            get;
+            set;
+        }
+
+        [CsvColumnAttribute(2, 100)]
+        public int Hp;
+
+        public override string ToString() {
+            return string.Format("Id={0}, Name={1}, HP={2}", id, Name, Hp);
+        }
+    }
+
+    void Start() {
+        //readerをList<TestEnemyData>化して、それぞれ表示
+        using (var reader = new CSVReader<Player_Data>("csv/Actor/Player/Player", true)) {
+            reader.ToList().ForEach(player => Debug.Log(player.ToString()));
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
